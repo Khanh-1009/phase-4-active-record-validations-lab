98968d55c54b9226392148b3060e6ebffde4e011
Khanh Tran <katetrannbkhanh@gmail.com>
2023-08-16 13:31:59 -0700

    commit tree 521be23b143e977aa17b1a5535c5d3653a11daf6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :acceptable_title
+
+    def acceptable_title
+        if title == "True Facts"
+            errors.add(:title, "Won't Believe")
+        end
+    end
 end
